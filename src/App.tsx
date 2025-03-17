e5db4cd1e436670e5bca4562552c85cf39eb563a
Hyungeun Jo <geunee92@ship-da.com>
2025-03-17 22:56:35 +0900

    feat: button 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Text from '@shared/Text'
+import Button from './components/shared/Button'
 
 function App() {
   return (
@@ -13,6 +14,22 @@ function App() {
       <Text typography="t3">t3</Text>
       <Text typography="t4">t4</Text>
       <Text>t5</Text>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error">클릭해주세요</Button>
+      <Button color="success" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button full={true}>클릭해주세요</Button>
+      <Button full={true} disabled={true}>
+        클릭해주세요
+      </Button>
     </div>
   )
 }
